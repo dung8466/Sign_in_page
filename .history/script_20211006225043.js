3b33c7ddcfea8e89735b9fec0685d0cef2447b78
dung8466 <64646403+dung8466@users.noreply.github.com>
2021-10-06 23:04:39 +0700

    Add html css js

---
+++ .history/script_20211006225043.js
@@ -0,0 +1,11 @@
+var x= document.getElementById("signin");
+var y= document.getElementById("register");
+var z= document.getElementById("btn");
+
+function register(){
+    x.style.left = "-480px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+
---
+++ .history/script_20211006225601.js
@@ -0,0 +1,16 @@
+var x= document.getElementById("signin");
+var y= document.getElementById("register");
+var z= document.getElementById("btn");
+
+function register(){
+    x.style.left = "-480px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function signin(){
+    x.style.left = "50px";
+    y.style.left = "480px";
+    z.style.left = "0px";
+}
+
